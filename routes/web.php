98a337731eaaa9b498f6136a17180a454abbd4dd
Fauzan Azima <fauzannazimaaa123@gmail.com>
2024-10-18 04:06:36 +0700

    menginstall laravel ui

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
-use App\Http\Controllers\categoryController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\backend\categoryController;
 
 Route::get('/', function () {
     return view('index');
 });
 
 Route::get('/serverside', [categoryController::class, 'getData'])->name('serverside');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
